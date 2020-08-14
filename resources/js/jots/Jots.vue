6bb3926d14758407254d66a449da378c1877c5f0
nathan hishon <nathan.hishon@gmail.com>
2020-08-13 19:26:21 -0500

    jot count3

--- resources/js/jots/Jots.vue
+++ resources/js/jots/Jots.vue
@@ -14,7 +14,7 @@
                     <span>{{getJotType(data.value)}}</span>
                 </template>
 
-                <template v-slot:cell(jotCount)="data">
+                <template v-slot:cell(jot_count)="data">
                     <b-badge variant="primary" class="mr-1">
                         {{data.value}}
                     </b-badge>
