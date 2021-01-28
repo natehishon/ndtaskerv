cb1f806555fa0b57f33f31c8937fa50b0743df97
nathan hishon <nathan.hishon@gmail.com>
2021-01-27 22:42:45 -0600

    buttons

--- resources/js/auth/SignIn.vue
+++ resources/js/auth/SignIn.vue
@@ -36,11 +36,11 @@
                         </b-form-group>
 
                         <div class="sign-in-buttons">
-                            <b-button size="lg" type="submit" class="mb-3 mr-2" variant="info">
-                                <i class="fas fa-door-open"></i>&nbsp;sign in
-                            </b-button>
                             <b-button size="lg" @click="signUp" class="mb-3" variant="success">
                                 <i class="fas fa-user-plus"></i>&nbsp;sign up
+                            </b-button>
+                            <b-button size="lg" type="submit" class="mb-3 mr-2" variant="info">
+                                <i class="fas fa-door-open"></i>&nbsp;sign in
                             </b-button>
                         </div>
 
